a45bffed540dc6f6dd578b261357b4811dc2e5b0
Willner <500196859@fvtc.edu>
2024-04-30 07:48:01 -0500

    adding

---
+++ site.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const {MongoClient, ObjectId} = require('mongodb')
+const {url} = process.env.MONGODB_URL || require('./secrets/mongo.json');
+
+const getCollection = async (dbName, collectionName) => {
+    await client.connect();
+    return client.db(dbname).collection(collectionName);
+}
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+const root = path.join(__dirname, 'public');
+
+app.use(express.json());
+app.use(express.static('public'));
