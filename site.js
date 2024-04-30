42d8b91cff3edb9d93f3862cffde3ac4d41a4017
Claire Fischer <500191607@fvtc.edu>
2024-04-30 08:51:59 -0500

    adding

--- site.js
+++ site.js
@@ -1,12 +1,19 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const { getCollection, ObjectId } = require('./db.js');
+const { getCollection, ObjectId } = require('./db.js')
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001
 
-const root = path.join(__dirname, 'public');
+const root = path.join(__dirname, 'public')
 
-app.use(express.json());
-app.use(express.static('public'));
+app.use(express.json())
+app.use(express.static('public'))
+
+
+app.get('/api/menu', (request, response)=> {
+    const collection = getCollection('Final-API', 'menu')
+    const menu = collection.find().toArray()
+    response.json(menu)
+})
